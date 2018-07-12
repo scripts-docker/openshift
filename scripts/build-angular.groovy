ccce0a14fc0edb34b209d8aae42d2bd5dd233829
Marcelo Lúcio <marcelo.lucio1982@gmail.com>
2018-07-12 16:32:13 -0300

    ssss

--- scripts/build-angular.groovy
+++ scripts/build-angular.groovy
@@ -64,7 +64,7 @@ podTemplate(label: label, cloud: 'openshift', containers: [
           }
 
           stage ('sonar') {
-
+              sh("sonar-scanner")
               sh("sonar-scanner -Dsonar.host.url=http://172.30.11.175:9000 -Dsonar.projectKey=${jsonMap.name} -Dsonar.projectName='${jsonMap.description}' -Dsonar.projectVersion=${VERSAO}")
           }
 

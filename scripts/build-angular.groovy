38f4124d9f1fe6187a120f49c0527e1edfff5f92
Marcelo Lúcio <marcelo.lucio1982@gmail.com>
2018-07-13 17:06:40 -0300

    script s

--- scripts/build-angular.groovy
+++ scripts/build-angular.groovy
@@ -24,60 +24,7 @@ podTemplate(label: label, cloud: 'openshift', containers: [
             jsonMap.version = "${VERSAO}"
             
           }
-          stage ('install modules'){
-            
-              sh '''
-                npm install
-              '''
-            
-          }
-          stage ('test'){
-            
-              sh '''
-                $(npm bin)/ng test --single-run --code-coverage --browsers Chrome_no_sandbox
-              '''
-            
-          }
-          stage ('code quality'){
-            
-              sh '$(npm bin)/ng lint'
-            
-          }
-
-          stage ('sonar') {
-              sh("sonar-scanner -Dsonar.tests=src/app -Dsonar.test.inclusions=**/*.spec.ts -Dsonar.ts.tslint.configPath=tslint.json -Dsonar.sources=src/app -Dsonar.exclusions=**/node_modules/**,**/*.spec.ts -Dsonar.host.url=${SONAR} -Dsonar.typescript.lcov.reportPaths=coverage/lcov.info -Dsonar.projectKey=${jsonMap.name} -Dsonar.projectName='${jsonMap.description}' -Dsonar.projectVersion=${VERSAO}")
-          }
-
-          stage ('build') {
-            
-              sh '$(npm bin)/ng build --prod --build-optimizer'
-            
-          }
-
-          stage ('publica nexus') {
-
-            def json = JsonOutput.toJson(jsonMap)
-            json = JsonOutput.prettyPrint(json)
-
-            writeFile(file:'package.json', text: json)
-
-            def npmPublico = "registry=${NEXUS_NPM_PUBLICO}"
-            def npmPrivado = "//${NEXUS_NPM_PRIVADO}:_authToken=${NEXUS_NPM_TOKEN}"
-            
-            sh "echo -e '$npmPublico\n$npmPrivado\nstrict-ssl=false\nalways-auth=true' > $HOME/.npmrc"
-            
-            sh '''
-                cp package.json dist/
-                cd dist && npm publish
-              '''            
-          }
-
-          stage ('build/deploy image') {
-            
-            sh "oc start-build ${NOME_APLICACAO}  -e VERSAO_APLICACAO=${VERSAO} --build-arg URL_ARTEFATO_DOWNLOAD=http://${NEXUS_NPM_PRIVADO}${NOME_APLICACAO}/-/${NOME_APLICACAO}-${VERSAO}.tgz --build-arg ARTEFATO=${NOME_APLICACAO}-${VERSAO}.tgz --follow=true"
-            sh "oc tag ${NOME_APLICACAO}:latest ${NOME_APLICACAO}:${VERSAO}"
-            
-          }
+          
                   
       }
     
